4abefe8b87cd9b5cea3f13c755bb2ea910dcb6b2
Michelle <wuebben.michelle@yahoo.com>
2019-09-16 19:34:47 -0500

    added more users

--- app/data/friends.js
+++ app/data/friends.js
@@ -688,8 +688,8 @@ var friends = [
     ]
     },
     {
-    "name": "Chynga",
-    "photo": "https://images2.minutemediacdn.com/image/upload/c_crop,h_1193,w_2121,x_0,y_64/f_auto,q_auto,w_1100/v1565279671/shape/mentalfloss/578211-gettyimages-542930526.jpg",
+        "name": "boyang matsapola",
+        "photo": "https://miro.medium.com/max/736/1*1e8i4KFLv8IOBTkKtCD08A.jpeg",
     "scores": [
     "2",
     "3",
@@ -720,8 +720,8 @@ var friends = [
     ]
     },
     {
-    "name": "Chynga",
-    "photo": "https://images2.minutemediacdn.com/image/upload/c_crop,h_1193,w_2121,x_0,y_64/f_auto,q_auto,w_1100/v1565279671/shape/mentalfloss/578211-gettyimages-542930526.jpg",
+        "name": "boyang matsapola",
+        "photo": "https://miro.medium.com/max/736/1*1e8i4KFLv8IOBTkKtCD08A.jpeg",
         "scores": [
     "2",
     "3",
@@ -736,8 +736,8 @@ var friends = [
     ]
     },
     {
-    "name": "Chynga",
-    "photo": "https://images2.minutemediacdn.com/image/upload/c_crop,h_1193,w_2121,x_0,y_64/f_auto,q_auto,w_1100/v1565279671/shape/mentalfloss/578211-gettyimages-542930526.jpg",
+        "name": "boyang matsapola",
+        "photo": "https://miro.medium.com/max/736/1*1e8i4KFLv8IOBTkKtCD08A.jpeg",
     "scores": [
     "2",
     "3",
